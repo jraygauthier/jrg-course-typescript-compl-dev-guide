7c97cfe27e1926726711c9c84ebdfaace1a51f03
Raymond Gauthier <jraygauthier@gmail.com>
2022-10-17 16:20:09 -0400

    Update

---
+++ s10-77-app/sort/src/index.ts
@@ -0,0 +1,15 @@
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
+
+/*
+const numbersCollection = new NumbersCollection([100000, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data);
+*/
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
