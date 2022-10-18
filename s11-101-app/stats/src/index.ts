39ee8b1512b11794d12376d1116491d5d98444cf
Raymond Gauthier <jraygauthier@gmail.com>
2022-10-18 15:29:21 -0400

    Update

--- s11-101-app/stats/src/index.ts
+++ s11-101-app/stats/src/index.ts
@@ -1,8 +1,15 @@
-import { MatchReader } from './MatchReader'
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+
+// Create an object that satisfiles the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv')
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 /*
 const dateOfFirstMatch = reader.data[0][0];
@@ -25,7 +32,7 @@ const draw = 'D';
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   }
--- s11-101-app/stats/src/inheritance/CsvFileReader.ts
+++ s11-101-app/stats/src/inheritance/CsvFileReader.ts
@@ -1,11 +1,13 @@
 
 import fs from 'fs';
 
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {
   }
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
@@ -15,6 +17,7 @@ export class CsvFileReader {
     .map(
       (row: string): string[] => {
       return row.split(',');
-    });
+    })
+    .map(this.mapRow)
   }
 }
